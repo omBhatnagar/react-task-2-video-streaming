648193ac09344f0fa3d9e1809271e2bc009e66e1
Om Bhatnagar <omishb2001@gmail.com>
2023-03-30 21:48:10 +0530

    Setup tailwind

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -4,10 +4,11 @@ const API_KEY = process.env.REACT_APP_YOUTUBE_API_ACCESS_KEY;
 
 export const getRecommended = async () => {
 	try {
-		const response = await axios.get(
-			`https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=9&regionCode=PK&key=${API_KEY}`,
-		);
-		return response;
+		// const response = await axios.get(
+		// 	`https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=9&regionCode=PK&key=${API_KEY}`,
+		// );
+		// return response;
+		return [];
 	} catch (error) {
 		console.log(error);
 		return [];
@@ -16,10 +17,11 @@ export const getRecommended = async () => {
 
 export const getChannelDetails = async (channelId) => {
 	try {
-		const response = await axios.get(
-			`https://www.googleapis.com/youtube/v3/channels?part=snippet&id=${channelId}&key=${API_KEY}`,
-		);
-		return response;
+		// const response = await axios.get(
+		// 	`https://www.googleapis.com/youtube/v3/channels?part=snippet&id=${channelId}&key=${API_KEY}`,
+		// );
+		// return response;
+		return [];
 	} catch (error) {
 		console.log(error);
 		return [];
