c0f93cab49f4ee8a8a042c2476f28ff467f31e77
Om Bhatnagar <omishb2001@gmail.com>
2023-03-31 19:34:10 +0530

    Added search on pressing enter key.

--- src/components/searchbar/searchbar.jsx
+++ src/components/searchbar/searchbar.jsx
@@ -1,17 +1,28 @@
 import { useState } from "react";
 import { BsSearch } from "react-icons/bs";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const SearchBar = () => {
+	const navigate = useNavigate();
+
 	const [searchQuery, setSearchQuery] = useState("");
 
+	const redirect = (e) => {
+		if (e.key !== "Enter") return;
+		navigate(`/search/${searchQuery}`);
+	};
+
 	return (
-		<div className='flex justify-between items-stretch w-1/2 mt-3 mb-6 rounded-3xl border border-[#303030] overflow-hidden'>
+		<div
+			onClick={redirect}
+			className='flex justify-between items-stretch w-1/2 mt-3 mb-6 rounded-3xl border border-[#303030] overflow-hidden'
+		>
 			<input
 				type='text'
 				className='bg-[#121212] flex-grow pl-4 py-2 text-gray-300 focus:outline-none'
 				placeholder='Search'
 				value={searchQuery}
+				onKeyDown={redirect}
 				onChange={(e) => setSearchQuery(e.target.value)}
 			/>
 			<Link
